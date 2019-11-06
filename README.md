f3e3469dbad3e86267081231e0f887d01f75fe66
unbelt <unbelt@outlook.com>
2019-11-06 23:42:04 +0200

    Add firebase hosting configs

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ Run `yarn start` to build and start a dev server. Navigate to `http://localhost:
 
 Run `yarn build` to build the project. The build artifacts will be stored in the `dist` directory.
 Use the `yarn build:prod` for a production build.
+
+### Online demo
+
+https://utils-belt.firebaseapp.com
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
